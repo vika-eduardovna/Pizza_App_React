633c126759e4b88a602c8188da4f527d720e8288
Vika <viktoria.wout@gmail.com>
2023-01-28 21:27:44 +0100

    styles added app.scss, header.scss

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import Categories from "../Categories/Categories";
+import Header from "../Header/Header";
+import './app.scss'
+
+function App() {
+  return (
+    <div >
+      <Header/>
+      <Categories/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Categories() {
+  return (
+    <div className='categories'>
+        <ul>
+            <li className='active'> All</li>
+            <li>Meat</li>
+            <li>Vegan</li>
+            <li>Grill</li>
+            <li>Chili</li>
+        </ul>
+    </div>
+  )
+}
