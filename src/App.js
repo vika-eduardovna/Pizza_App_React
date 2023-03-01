d7a62743aa714df4ba3b1511de1f0fa1d4419214
Vika <viktoria.wout@gmail.com>
2023-03-01 15:45:36 +0100

    Home page and NotFound page created using react router dom

--- src/App.js
+++ src/App.js
@@ -1,41 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import Categories from './components/Categories';
+import React from 'react';
 import Header from './components/Header';
-import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock/PizzaBlock';
-import Skeleton from './components/PizzaBlock/Skeleton';
 import './scss/app.scss'
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 
 function App() {
-
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    fetch('https://63ff2b80571200b7b7d749c8.mockapi.io/pizzas')
-      .then(res => res.json())
-      .then(json => setItems(json));
-    setIsLoading(!isLoading);
-  }, []); //empty array is component did mount
 
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content_top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="title">All pizzas</h2>
-          <div className="items">
-            {
-              isLoading
-                ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-                : items.map(el => <PizzaBlock key={el.id} {...el} />)
-            }
-          </div>
+          {/* <Home /> */}
+          <NotFound/>
         </div>
       </div>
     </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+
+export default function Home() {
+    const [items, setItems] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('https://63ff2b80571200b7b7d749c8.mockapi.io/pizzas')
+            .then(res => res.json())
+            .then(json => setItems(json));
+        setIsLoading(!isLoading);
+    }, []); //empty array is component did mount
+
+    return (
+        <>
+            <div className="content_top">
+                <Categories />
+                <Sort />
+            </div>
+            <h2 className="title">All pizzas</h2>
+            <div className="items">
+                {
+                    isLoading
+                        ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+                        : items.map(el => <PizzaBlock key={el.id} {...el} />)
+                }
+            </div>
+        </>
+    )
+}
