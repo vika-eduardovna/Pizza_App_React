5a3b1d063d44f10756c17fb111508e7769f45126
Vika <viktoria.wout@gmail.com>
2023-03-01 13:17:48 +0100

    fetcing data from mockapi

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
+import React, { useEffect, useState } from 'react';
 import Categories from './components/Categories';
 import Header from './components/Header';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
-import pizzas from './assets/data.json'
 import './scss/app.scss'
 
 
 function App() {
+
+  const [items, setItems] = useState([]);
+  
+  useEffect(() => {
+    fetch('https://63ff2b80571200b7b7d749c8.mockapi.io/pizzas')
+      .then(res => res.json())
+      .then(json => setItems(json));
+  }, []); //empty array is component did mount
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
           <div className="content_top">
-            <Categories/>
-            <Sort/>
+            <Categories />
+            <Sort />
           </div>
           <h2 className="title">All pizzas</h2>
           <div className="items">
-           {
-            pizzas.map(el =>  <PizzaBlock key={el.id} {...el}/>)
-           }
+            {
+              items.map(el => <PizzaBlock key={el.id} {...el} />)
+            }
           </div>
         </div>
       </div>
